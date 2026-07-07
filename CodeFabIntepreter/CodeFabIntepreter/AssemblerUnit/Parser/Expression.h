c56cbec9bd94dc66ccf989e42c90b53048368384
Sangwon Kim <brave_bean@naver.com>
2026-07-07 12:46:11 +0900

    [feature] Add AssemblerUnit Parser with Expression and Statement nodes
    Move Statement/Expression/Token node definitions out of Node.h into
    dedicated Parser headers and add the Parser class skeleton.

---
+++ CodeFabIntepreter/CodeFabIntepreter/AssemblerUnit/Parser/Expression.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "../../Node.h"
+
+class ExpressionOrToken : public Node {
+};
+
+class Expression : public ExpressionOrToken {
+private:
+	vector<ExpressionOrToken*> children;
+};
+
+class LiteralExpr : public Expression {
+};
+
+class VariableExpr : public Expression {
+};
+
+class AssignExpr : public Expression {
+};
+
+class BinaryExpr : public Expression {
+};
+
+class UnaryExpr : public Expression {
+};
+
+class GroupingExpr : public Expression {
+};
+
+class LogicalExpr : public Expression {
+};
