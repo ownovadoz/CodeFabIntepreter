921bab199359aa90b1033e7b3a4c1095e02f2d9f
Soyo-Lee <soyokaze86@naver.com>
2026-07-08 07:49:24 +0900

    [FeatureImplement] Environment get/assign이 enclosing 스코프 체인을 탐색하도록 구현
    - 현재 스코프에 변수가 없으면 enclosing 환경을 재귀적으로 탐색
    - assign이 값을 갱신한 뒤 return 하지 않고 항상 예외를 던지던 버그 수정

--- CodeFabIntepreter/CodeFabIntepreter/ExecutorUnit/Environment.cpp
+++ CodeFabIntepreter/CodeFabIntepreter/ExecutorUnit/Environment.cpp
@@ -15,12 +15,23 @@ optional<Value> Environment::get(const string& name) const
     auto found = values.find(name);
     if (found != values.end()) return found->second;
 
+    if (enclosing != nullptr) return enclosing->get(name);
+
     throw RuntimeError(name, "Undefined variable '" + name + "'.");
 }
 
 void Environment::assign(const string& name, const Value& value)
 {
     auto found = values.find(name);
-    if (found != values.end()) found->second = value;
+    if (found != values.end()) {
+        found->second = value;
+        return;
+    }
+
+    if (enclosing != nullptr) {
+        enclosing->assign(name, value);
+        return;
+    }
+
     throw RuntimeError(name, "Undefined variable '" + name + "'.");
 }
