9275570d36ecdba8ea3e5091112eaca5d04fc601
Soyo-Lee <soyokaze86@naver.com>
2026-07-07 22:26:29 +0900

    [feature] add Environment class/test file, update VS project file

---
+++ CodeFabIntepreter/CodeFabIntepreter/ExecutorUnit/Environment.cpp
@@ -0,0 +1,5 @@
+#include "Environment.h"
+
+Environment::Environment(Environment* enclosing)
+{
+}
---
+++ CodeFabIntepreter/CodeFabIntepreter/ExecutorUnit/EnvironmentTest.cpp
@@ -0,0 +1,8 @@
+#include "Environment.h"
+
+#include <gmock/gmock.h>
+
+TEST(EnvironmentTest, DefinedVariableIsReadable)
+{
+    Environment env;
+}
