1428b103be8b133291757a9bda71133cb9f13065
hyunji-jeon-ss <hj_ash@naver.com>
2026-07-07 09:39:24 +0900

    [feature] EmptyLine Test

--- CodeFabIntepreter/CodeFabIntepreter/PromptShellTest.cpp
+++ CodeFabIntepreter/CodeFabIntepreter/PromptShellTest.cpp
@@ -5,12 +5,29 @@
 #include <string>
 using std::string;
 
-TEST(PromptShellTest, BasicFunctionality) {
+TEST(PromptShellTest, NormalLineTest) {
 	// Test the basic functionality of the PromptShell class
 	std::istringstream inputStream;
 	std::streambuf* originalCinBuffer = nullptr;
 	originalCinBuffer = std::cin.rdbuf();
 	string input = "var x = 10;";
+	inputStream.str(input);
+	inputStream.clear();
+	std::cin.rdbuf(inputStream.rdbuf());
+
+	PromptShell shell;
+	shell.runPrompt();
+
+	EXPECT_EQ(input, shell.getLine());
+	std::cin.rdbuf(originalCinBuffer);
+}
+
+TEST(PromptShellTest, EmptyLineTest) {
+	// Test the basic functionality of the PromptShell class
+	std::istringstream inputStream;
+	std::streambuf* originalCinBuffer = nullptr;
+	originalCinBuffer = std::cin.rdbuf();
+	string input = "";
 	inputStream.str(input);
 	inputStream.clear();
 	std::cin.rdbuf(inputStream.rdbuf());
