3a2f650fc8f9bb4a3878cc2190290daef138d850
junyong.uhm <junyong.uhm@gmail.com>
2026-07-07 13:00:36 +0900

    [feature] Variable Scope Check 에 대한 구현

--- CodeFabIntepreter/CodeFabIntepreter/CheckerUnit/VariableScopeChecker.cpp
+++ CodeFabIntepreter/CodeFabIntepreter/CheckerUnit/VariableScopeChecker.cpp
@@ -14,18 +14,32 @@ CheckResult CheckResult::checkerUnitHasError(std::string message)
 
 void VariableScopeChecker::enterScope()
 {
+    scope_stack.emplace_back();
 }
 
 void VariableScopeChecker::exitScope()
 {
+    if (scope_stack.empty()) return;
+
+    scope_stack.pop_back();
 }
 
 CheckResult VariableScopeChecker::declareVariable(const std::string& name, const std::vector<std::string>& initializer_references)
 {
+    if (isDeclaredInCurrentScope(name))
+        return CheckResult::checkerUnitHasError("이미 해당 변수는 현재 스코프에서 사용중입니다: '" + name + "'");
+
+    bool is_self_referenced = std::find(initializer_references.begin(), initializer_references.end(), name) != initializer_references.end();
+    if (is_self_referenced)
+        return CheckResult::checkerUnitHasError("자신의 초기화식에서 지역변수를 읽을 수 없습니다: '" + name + "'");
+
+    scope_stack.back().insert(name);
     return CheckResult::isOK();
 }
 
 bool VariableScopeChecker::isDeclaredInCurrentScope(const std::string& name) const
 {
-    return 0;
+    if (scope_stack.empty()) return false;
+
+    return scope_stack.back().count(name) > 0;
 }
