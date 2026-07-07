b44d75d74521a821f2821512d2e4d3c2466dda29
Sangwon Kim <brave_bean@naver.com>
2026-07-07 12:46:11 +0900

    [feature] Add AssemblerUnit Parser with Expression and Statement nodes
    Move Statement/Expression/Token node definitions out of Node.h into
    dedicated Parser headers and add the Parser class skeleton.

---
+++ CodeFabIntepreter/CodeFabIntepreter/AssemblerUnit/Parser/Parser.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "../../Node.h"
+#include "../Tokenize/Token.h"
+
+#include <vector>
+
+using std::vector;
+
+class Parser
+{
+public:
+	Node* parse(vector<Token> tokens);
+
+private:
+
+};
--- CodeFabIntepreter/CodeFabIntepreter/Node.h
+++ CodeFabIntepreter/CodeFabIntepreter/Node.h
@@ -10,32 +10,3 @@ interface Node {
 
 	virtual void accept() = 0;
 };
-
-class Statement : public Node {
-public:
-	void accept() override;
-
-private:
-	vector<Node*> children;
-};
-
-class ExpressionOrToken : public Node
-{
-public:
-	void accept() override;
-};
-
-class Expression : public ExpressionOrToken
-{
-public:
-	void accept() override;
-
-private:
-	vector<ExpressionOrToken*> children;
-};
-
-class Token : public ExpressionOrToken
-{
-public:
-	void accept() override;
-};
