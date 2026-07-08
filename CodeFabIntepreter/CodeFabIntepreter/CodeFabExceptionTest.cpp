b0ea77c39c28a296f65377e60322e3b922b8fb0c
hyunji-jeon-ss <hj_ash@naver.com>
2026-07-08 10:54:38 +0900

    [feature] Add unit Test #2

--- CodeFabIntepreter/CodeFabIntepreter/CodeFabExceptionTest.cpp
+++ CodeFabIntepreter/CodeFabIntepreter/CodeFabExceptionTest.cpp
@@ -9,3 +9,15 @@ TEST(CodeFabExceptionTest, LineConstructorFormatsMessageWithLineNumber) {
 	EXPECT_EQ(error.getMessage(), "unexpected character");
 	EXPECT_STREQ(error.what(), "[line 7] Error: unexpected character");
 }
+
+TEST(CodeFabExceptionTest, IsCatchableAsStdException) {
+	try {
+		throw CodeFabException(1, "boom");
+	}
+	catch (const std::exception& e) {
+		EXPECT_STREQ(e.what(), "[line 1] Error: boom");
+		return;
+	}
+
+	FAIL() << "CodeFabException should be catchable as std::exception";
+}
