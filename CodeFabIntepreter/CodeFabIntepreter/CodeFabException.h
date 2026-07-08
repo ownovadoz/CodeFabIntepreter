de52f26266e27ce0e8285938853cabe9dac2e980
hyunji-jeon-ss <hj_ash@naver.com>
2026-07-08 11:08:03 +0900

    [feature] Add Exception with Token struct

--- CodeFabIntepreter/CodeFabIntepreter/CodeFabException.h
+++ CodeFabIntepreter/CodeFabIntepreter/CodeFabException.h
@@ -12,12 +12,19 @@ public:
         : std::runtime_error(formatMessage(line, message)), line(line), message(message) {
     }
 
+    CodeFabException(const Token& token, const string& message)
+        : std::runtime_error(formatMessage(token, message)), line(token.getLine()), message(message) {
+    }
+
     int getLine() const { return line; }
     const string& getMessage() const { return message; }
 
 private:
     static string formatMessage(int line, const string& message) {
         return "[line " + std::to_string(line) + "] Error: " + message;
+    }
+    static string formatMessage(const Token& token, const string& message) {
+        return "[line " + std::to_string(token.getLine()) + "] Type : " + tokenTypeToString(token.getType()) + ", Lexeme : " + token.getLexeme() + ", literal : " + stringify(token.getLiteral()) + ", message : " + message;
     }
     int line;
     string message;
--- CodeFabIntepreter/CodeFabIntepreter/CodeFabExceptionTest.cpp
+++ CodeFabIntepreter/CodeFabIntepreter/CodeFabExceptionTest.cpp
@@ -21,3 +21,12 @@ TEST(CodeFabExceptionTest, IsCatchableAsStdException) {
 
 	FAIL() << "CodeFabException should be catchable as std::exception";
 }
+
+TEST(CodeFabExceptionTest, TokenConstructorFormatsMessageWithLexeme) {
+	Token token(TokenType::SEMICOLON, ";", std::monostate{}, 12);
+	CodeFabException error(token, "expected expression");
+
+	EXPECT_EQ(error.getLine(), 12);
+	EXPECT_EQ(error.getMessage(), "expected expression");
+	EXPECT_STREQ(error.what(), "[line 12] Type : SEMICOLON, Lexeme : ;, literal : nil, message : expected expression");
+}
