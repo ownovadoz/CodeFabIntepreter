3fba2e44dc557819b6c68a6b1192369ad6655516
junyong.uhm <junyong.uhm@gmail.com>
2026-07-08 10:50:21 +0900

    [FeatureImplement] checker: BlockStmt/VarDeclareStmt를 순회하며 스코프 검사 수행
    check()이 전역 스코프에 진입한 뒤 checkStatement로 위임하고,
    BlockStmt는 새 스코프를 열고 자식 문장을 순서대로 검사하며,
    VarDeclareStmt는 VariableScopeChecker.declareVariable을 호출해
    중복 선언 여부를 판정하도록 구현한다.

--- CodeFabIntepreter/CodeFabIntepreter/CheckerUnit/SemanticChecker.cpp
+++ CodeFabIntepreter/CodeFabIntepreter/CheckerUnit/SemanticChecker.cpp
@@ -2,20 +2,42 @@
 
 CheckResult SemanticChecker::check(Statement* root)
 {
-    return CheckResult::isOK();
+    scope_checker.enterScope();
+    return checkStatement(root);
 }
 
 CheckResult SemanticChecker::checkStatement(Statement* stmt)
 {
+    if (stmt == nullptr) return CheckResult::isOK();
+
+    if (BlockStmt* block = dynamic_cast<BlockStmt*>(stmt))
+        return checkBlockStmt(block);
+
+    if (VarDeclareStmt* var_decl = dynamic_cast<VarDeclareStmt*>(stmt))
+        return checkVarDeclareStmt(var_decl);
+
     return CheckResult::isOK();
 }
 
 CheckResult SemanticChecker::checkBlockStmt(BlockStmt* block)
 {
+    scope_checker.enterScope();
+
+    for (Statement* stmt : block->getStatements())
+    {
+        CheckResult result = checkStatement(stmt);
+        if (result.hasError)
+        {
+            scope_checker.exitScope();
+            return result;
+        }
+    }
+
+    scope_checker.exitScope();
     return CheckResult::isOK();
 }
 
 CheckResult SemanticChecker::checkVarDeclareStmt(VarDeclareStmt* var_decl)
 {
-    return CheckResult::isOK();
+    return scope_checker.declareVariable(var_decl->getName().getLexeme(), {});
 }
