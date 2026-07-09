217c1e1cdbf4138e25423561e2ac0ef87a8bdaa1
Soyo-Lee <soyokaze86@naver.com>
2026-07-09 16:48:10 +0900

    [FeatureImplement] Interpreter가 ExprVisitor/StmtVisitor 순수 가상 함수를 구현
    Interpreter가 ExprVisitor/StmtVisitor를 상속하면서 추상 클래스가 되어
    컴파일이 되지 않던 것을 13개 visitXxx를 모두 구현해 해결한다.
    
    - execute()/evaluate()의 dynamic_cast 분기 체인을 stmt->accept(*this) /
      expr->accept(*this) 기반 디스패치로 교체(Visitor.cpp에 이미 구현된
      accept()가 각 visitXxx로 위임한다).
    - 이미 실행/평가 로직이 있던 6개 Stmt(Block/VarDeclare/Expression/Print/
      If/For)와 2개 Expr(Literal/Variable)의 visitXxx는 기존
      executeXxx/evaluateXxx 헬퍼로 그대로 위임하는 얇은 래퍼로 구현했다.
      Stmt 쪽은 accept()가 const Xxx&를 주므로 Checker.cpp와 동일한
      const_cast 패턴으로 헬퍼에 넘긴다.
    - evaluate()는 has_evaluation_result 플래그로 "아무 visitor도 값을
      세팅하지 않으면 미지원 표현식"임을 판단하도록 evaluation_result/
      has_evaluation_result 멤버를 추가했다.
    - AssignExpr/BinaryExpr/UnaryExpr/GroupingExpr/LogicalExpr은 아직 이
      브랜치에 실행 로직이 없어 빌드 에러 방지용 빈 구현({})으로만 남겼다.
    
    모든 InterpreterTest 케이스(기존 + accept() 직접 호출 테스트)가
    통과한다(green).

--- CodeFabIntepreter/CodeFabIntepreter/ExecutorUnit/Interpreter.cpp
+++ CodeFabIntepreter/CodeFabIntepreter/ExecutorUnit/Interpreter.cpp
@@ -26,35 +26,7 @@ void Interpreter::execute(Statement* stmt)
 {
     if (stmt == nullptr) return;
 
-    if (BlockStmt* block = dynamic_cast<BlockStmt*>(stmt)) {
-        executeBlockStmt(block);
-        return;
-    }
-
-    if (VarDeclareStmt* var_decl = dynamic_cast<VarDeclareStmt*>(stmt)) {
-        executeVarDeclareStmt(var_decl);
-        return;
-    }
-
-    if (ExpressionStmt* expr_stmt = dynamic_cast<ExpressionStmt*>(stmt)) {
-        executeExpressionStmt(expr_stmt);
-        return;
-    }
-
-    if (PrintStmt* print_stmt = dynamic_cast<PrintStmt*>(stmt)) {
-        executePrintStmt(print_stmt);
-        return;
-    }
-
-    if (IfStmt* if_stmt = dynamic_cast<IfStmt*>(stmt)) {
-        executeIfStmt(if_stmt);
-        return;
-    }
-
-    if (ForStmt* for_stmt = dynamic_cast<ForStmt*>(stmt)) {
-        executeForStmt(for_stmt);
-        return;
-    }
+    stmt->accept(*this);
 }
 
 void Interpreter::executeBlockStmt(BlockStmt* block)
@@ -121,15 +93,12 @@ void Interpreter::executeForStmt(ForStmt* for_stmt)
 
 Value Interpreter::evaluate(const Expression* expr)
 {
-    if (const LiteralExpr* literal = dynamic_cast<const LiteralExpr*>(expr)) {
-        return evaluateLiteralExpr(literal);
-    }
+    has_evaluation_result = false;
+    expr->accept(*this);
 
-    if (const VariableExpr* variable = dynamic_cast<const VariableExpr*>(expr)) {
-        return evaluateVariableExpr(variable);
-    }
+    if (!has_evaluation_result) throw CodeFabException(0, "지원하지 않는 표현식입니다.");
 
-    throw CodeFabException(0, "지원하지 않는 표현식입니다.");
+    return evaluation_result;
 }
 
 Value Interpreter::evaluateLiteralExpr(const LiteralExpr* literal)
@@ -141,3 +110,51 @@ Value Interpreter::evaluateVariableExpr(const VariableExpr* variable)
 {
     return environment->get(variable->getToken().getLexeme());
 }
+
+void Interpreter::visitExpressionStmt(const ExpressionStmt& stmt)
+{
+    executeExpressionStmt(const_cast<ExpressionStmt*>(&stmt));
+}
+
+void Interpreter::visitIfStmt(const IfStmt& stmt)
+{
+    executeIfStmt(const_cast<IfStmt*>(&stmt));
+}
+
+void Interpreter::visitBlockStmt(const BlockStmt& stmt)
+{
+    executeBlockStmt(const_cast<BlockStmt*>(&stmt));
+}
+
+void Interpreter::visitVarDeclareStmt(const VarDeclareStmt& stmt)
+{
+    executeVarDeclareStmt(const_cast<VarDeclareStmt*>(&stmt));
+}
+
+void Interpreter::visitPrintStmt(const PrintStmt& stmt)
+{
+    executePrintStmt(const_cast<PrintStmt*>(&stmt));
+}
+
+void Interpreter::visitForStmt(const ForStmt& stmt)
+{
+    executeForStmt(const_cast<ForStmt*>(&stmt));
+}
+
+void Interpreter::visitLiteralExpr(const LiteralExpr& expr)
+{
+    evaluation_result = evaluateLiteralExpr(&expr);
+    has_evaluation_result = true;
+}
+
+void Interpreter::visitVariableExpr(const VariableExpr& expr)
+{
+    evaluation_result = evaluateVariableExpr(&expr);
+    has_evaluation_result = true;
+}
+
+void Interpreter::visitAssignExpr(const AssignExpr&) {}
+void Interpreter::visitBinaryExpr(const BinaryExpr&) {}
+void Interpreter::visitUnaryExpr(const UnaryExpr&) {}
+void Interpreter::visitGroupingExpr(const GroupingExpr&) {}
+void Interpreter::visitLogicalExpr(const LogicalExpr&) {}
--- CodeFabIntepreter/CodeFabIntepreter/ExecutorUnit/Interpreter.h
+++ CodeFabIntepreter/CodeFabIntepreter/ExecutorUnit/Interpreter.h
@@ -14,9 +14,9 @@ using std::string;
 using std::shared_ptr;
 
 #ifdef _DEBUG
-class Interpreter : public IExecutor
+class Interpreter : public IExecutor, public ExprVisitor, public StmtVisitor
 #else
-class Interpreter
+class Interpreter : public ExprVisitor, public StmtVisitor
 #endif
 {
 public:
@@ -42,7 +42,24 @@ private:
     Value evaluateLiteralExpr(const LiteralExpr* literal);
     Value evaluateVariableExpr(const VariableExpr* variable);
 
+    void visitExpressionStmt(const ExpressionStmt& stmt) override;
+    void visitIfStmt(const IfStmt& stmt) override;
+    void visitBlockStmt(const BlockStmt& stmt) override;
+    void visitVarDeclareStmt(const VarDeclareStmt& stmt) override;
+    void visitPrintStmt(const PrintStmt& stmt) override;
+    void visitForStmt(const ForStmt& stmt) override;
+
+    void visitLiteralExpr(const LiteralExpr& expr) override;
+    void visitVariableExpr(const VariableExpr& expr) override;
+    void visitAssignExpr(const AssignExpr& expr) override;
+    void visitBinaryExpr(const BinaryExpr& expr) override;
+    void visitUnaryExpr(const UnaryExpr& expr) override;
+    void visitGroupingExpr(const GroupingExpr& expr) override;
+    void visitLogicalExpr(const LogicalExpr& expr) override;
+
     shared_ptr<Environment> globals;
     shared_ptr<Environment> environment;
 
+    Value evaluation_result;
+    bool has_evaluation_result = false;
 };
